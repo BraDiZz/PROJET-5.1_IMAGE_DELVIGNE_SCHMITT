3f4082b44b83cf349a3df8b6e6f202ad1ed22d83
Vincent Schmitt <vs80labu@studserv.uni-leipzig.de>
2024-03-09 16:17:36 +0100

    Added multiple hues

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -4,19 +4,20 @@
 
 int main(int argc, char* argv[]){
     char inputFileName[250], outputFileName[250];
-    double hue;
+    double hue1, hue2;
       
-    if (argc != 4){
-        printf("Usage: ImageIn.ppm ImageOut.pgm hue\n"); 
+    if (argc != 5){
+        printf("Usage: ImageIn.ppm ImageOut.pgm hue1 hue2\n"); 
         exit (1) ;
     }
       
     sscanf (argv[1],"%s",inputFileName) ;
     sscanf (argv[2],"%s",outputFileName);
-    sscanf (argv[3],"%lf",&hue);
+    sscanf (argv[3],"%lf",&hue1);
+    sscanf (argv[4],"%lf",&hue2);
 
     ColorImage img(inputFileName);
-    ColorScheme scheme({hue});
+    ColorScheme scheme({hue1, hue2});
 
     ColorSchemeConverter::ConvertToColorScheme(img, scheme);
     img.Write(outputFileName);
