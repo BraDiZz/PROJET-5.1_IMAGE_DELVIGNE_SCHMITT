bddd0fdc756acc2350614798bb54489dc1af234e
Brian <brian.delvgne@gmail.com>
2024-03-22 14:29:24 +0100

    graphique appli

--- Code/src/HueSmother.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "ColorImage.h"
-
-class HueSmother {
-public:
-    static void SmoothHue(ColorImage &image, int radius);
-    static double GetAverageHue(const ColorImage &image, int x, int y, int radius);
-};
